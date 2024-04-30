4226b5e97bc8482a19a636f0557c5d3514c87342
Mateusbnl <mateusbnl@hotmail.com>
2024-04-30 20:15:24 -0300

    Refatoracao

--- src/batch/DTO/UsuarioDTO.java
+++
@@ -1,6 +0,0 @@
-package batch.DTO;
-
-import java.math.BigInteger;
-import java.time.LocalDate;
-
-public record UsuarioDTO(String nome, LocalDate data, BigInteger documento, int tipo){}
--- src/batch/ImportarClientesCsv.java
+++ src/batch/ImportarClientesCsv.java
@@ -1,96 +1,66 @@
 package batch;
 
-import batch.DTO.UsuarioDTO;
-import conta.Conta;
+
+
 import enums.ClassificacaoEnum;
 import services.CreditoService;
 import services.DebitoService;
 import services.UsuarioService;
 import usuario.Usuario;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.math.BigInteger;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
-import java.time.Year;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.time.temporal.ChronoUnit.YEARS;
 
 public class ImportarClientesCsv {
-    final Path path = Paths.get("C:\\docs\\pessoas.csv");
-    final Path pathCriados = Paths.get("C:\\docs\\resultado.csv");
-    final Pattern pattern = Pattern.compile(",");
+    final Path path = Paths.get("/home/mateusbnl/Documentos/pessoas.csv");
+    final Path pathCriados = Paths.get("/home/mateusbnl/Documentos/resultado"+LocalDate.now()+".csv");
     final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-    public List<UsuarioDTO> listarClientes() throws IOException {
-        List<UsuarioDTO> usuarios = new ArrayList<>();
-        try (Stream<String> lines = Files.lines(path)){
-            usuarios = lines.skip(1).map(line -> {
-                String[] arr = pattern.split(line);
-                return new UsuarioDTO(
-                        arr[0],
-                        LocalDate.parse(arr[1],formatter),
-                        new BigInteger(arr[2]),
-                        Integer.parseInt(arr[3]));
-            }).toList();
-        }
-        return usuarios;
+    public void importarClientes() throws IOException{
+        Stream<String> lines = Files.lines(path);
+        List<String> clientes = lines.skip(1)
+                .map(line -> line.split(","))
+                .filter(line -> verificaMaiorIdade(line))
+                .map(line -> UsuarioService.criarUsuario(new BigInteger(line[2]),defineClassificacao(line[3]),line[0]))
+                .map(usuario -> realizarDepositoMarketing(usuario))
+                .map(usuario -> {
+                    return usuario.getNome()+","+usuario.getId()+","+usuario.getClassificacao().toString()+","
+                    +usuario.getContas().get(0).getId()+","+usuario.getContas().get(0).saldo();
+                }).toList();
+
+        Files.write(pathCriados,clientes);
     }
 
-    public List<Usuario> criarClientes(List<UsuarioDTO> usuariosDTO, Usuario usuarioMarketing) throws IOException {
-        List<Usuario> listaCriados = new ArrayList<>();
-        usuariosDTO.
-                stream().
-                forEach(n -> {
-            if(n.tipo() == 2 && (YEARS.between(n.data(),LocalDate.now()) >= 18)){
-                Usuario u = UsuarioService.criarUsuario(n.documento(),ClassificacaoEnum.PF,n.nome());
-                DebitoService.realizarDebitoTransferencia(u.getContas().get(0),usuarioMarketing.getContas().get(0),50.00,usuarioMarketing,u);
-                listaCriados.add(u);
-            } else {
-                Usuario u = UsuarioService.criarUsuario(n.documento(),ClassificacaoEnum.PF,n.nome());
-                DebitoService.realizarDebitoTransferencia(u.getContas().get(0),usuarioMarketing.getContas().get(0),50.00,usuarioMarketing,u);
-                listaCriados.add(u);
-            }
-        });
-        return listaCriados;
-    }
-
-    public void gerarCsvContasCriadas(List<Usuario> usuariosComContas) throws IOException{
-        List<String[]> dataLines = new ArrayList<>();
-        usuariosComContas.stream().forEach(n -> dataLines.add(linhaClienteCriado(n)));
-        File csvOutputFile = new File("C:\\docs\\resultado-"+LocalDate.now()+".csv");
-        try (PrintWriter pw = new PrintWriter(csvOutputFile)) {
-            dataLines.stream()
-                    .map(this::convertToCSV)
-                    .forEach(pw::println);
+    public ClassificacaoEnum defineClassificacao(String valor){
+        if(valor == "1"){
+            return ClassificacaoEnum.PJ;
+        } else {
+            return ClassificacaoEnum.PF;
         }
     }
 
-    public String[] linhaClienteCriado(Usuario u){
-        String[] linha = new String[5];
-        linha[0] = u.getNome();
-        linha[1] = String.valueOf(u.getId());
-        linha[2] = u.getClassificacao().toString();
-        linha[3] = String.valueOf(u.getContas().get(0).getId());
-        linha[4] = String.valueOf(u.getContas().get(0).saldo());
-        return linha;
+    public boolean verificaMaiorIdade(String[] usuario){
+        if(YEARS.between(LocalDate.parse(usuario[1],formatter),LocalDate.now()) < 18 && usuario[3] == "2"){
+            return false;
+        } else {
+            return true;
+        }
     }
 
-    public String convertToCSV(String[] data) {
-        return Stream.of(data)
-                .collect(Collectors.joining(","));
+    public Usuario realizarDepositoMarketing(Usuario usuario){
+        Usuario usuarioMarketing = UsuarioService.criarUsuario(new BigInteger("99999999999999"),ClassificacaoEnum.PJ,"Conta do Marketing");
+        CreditoService.realizarCredito(usuarioMarketing.getContas().get(0),99999999999.00,usuarioMarketing,usuarioMarketing,"Campanha");
+        DebitoService.realizarDebitoTransferencia(usuario.getContas().get(0),usuarioMarketing.getContas().get(0),50.00,usuarioMarketing,usuario);
+        return usuario;
     }
-
-
 }
 
